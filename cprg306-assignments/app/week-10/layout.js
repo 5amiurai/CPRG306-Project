76d279bfe746384d3595836df767ef9dce047dba
Sami Rizk <sami.rizque@gmail.com>
2025-03-24 15:02:37 -0600

    week 10 assignmet added firestore to the shopping list app

---
+++ cprg306-assignments/app/week-10/layout.js
@@ -0,0 +1,17 @@
+import { AuthContextProvider } from "./_utils/auth-context";
+ 
+const Layout = ({ children }) => {
+  return (
+    <AuthContextProvider>
+      {children}
+    </AuthContextProvider>
+  );
+};
+ 
+export default Layout;
+
+
+
+
+
+

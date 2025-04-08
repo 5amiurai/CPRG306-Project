1d75288f634a2384d3072f8f38bf757920274b77
5amiurai <151583800+5amiurai@users.noreply.github.com>
2025-04-08 16:23:06 -0600

    Delete cprg306-assignments directory

--- cprg306-assignments/app/week-6/item.js
+++
@@ -1,7 +0,0 @@
-export default function Item({ name, quantity, category }) {
-    return (
-      <li className="p-2 mb-2 border">
-        <span>{name} - Quantity: {quantity} - Category: {category}</span>
-      </li>
-    );
-  }
